7c12b42fe32f868c5a0fe7323c6021a022065e22
Abhishek Poonia <abhishekpoonia22@gmail.com>
2017-01-01 22:40:44 +0530

    1. Unix env lookup trick!

---
+++ unix_env_lookup.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+print('''Instead of hardcoding python interpreter paths, use the environment variable (provided you have access to it).
+So if ever python moves to a new location, just update the env variable, not all scripts :)''')
